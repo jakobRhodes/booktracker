3ad379d5bbd8094a3ac1919ffdfa81dc04be33a4
jakobRhodes <46934743+jakobRhodes@users.noreply.github.com>
2020-03-12 16:00:28 -0600

    test

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ function addNewBookToDatabase (req, res) {
 const connectionString = process.env.DATABASE_URL || 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
 const pool = new Pool({connectionString: connectionString});
 var SQL_INSERT = "INSERT INTO Book (Title, Author, Genre) VALUES (" + req.query.bookTitle + "," + req.query.bookAuthor + "," + req.query.bookGenre + ")";
+console.log(SQL_INSERT);
 pool.query(SQL_INSERT, function(err, result) {
     // If an error occurred...
     if (err) {
@@ -22,9 +23,9 @@ pool.query(SQL_INSERT, function(err, result) {
         console.log(err);
     }
     // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
+    console.log("1 record inserted");
     console.log(result.rows);
  });
- 
+
 } 
 
