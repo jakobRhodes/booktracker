e02e61b7740475fff9a4a97fe9f87c375eefa578
jakobRhodes <46934743+jakobRhodes@users.noreply.github.com>
2020-03-11 21:18:35 -0600

    test

--- request.js
+++ request.js
@@ -6,11 +6,11 @@ app.set('views', 'public/views');
 app.set("view engine", "ejs");
 app.get('/addBook', testDatabaseConnection);
 app.listen(port, () => console.log(`Application is Listening on port ${port}!`));
-const connectionString = process.env.DATABASE_URL || 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
-const pool = new Pool({connectionString: connectionString});
-var sql = "SELECT * FROM Book";
 
 function testDatabaseConnection () {
+    const connectionString = 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
+const pool = new Pool({connectionString: connectionString});
+var sql = "SELECT * FROM Book";
 pool.query(sql, function(err, result) {
     // If an error occurred...
     if (err) {
