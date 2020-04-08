40742d5fcfd59bd2808419f3789ed5230179a9f3
jakobRhodes <46934743+jakobRhodes@users.noreply.github.com>
2020-04-08 02:34:56 -0600

    Finished Project

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const {Pool} = require("pg");
 const port = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', 'public/views');
 app.set("view engine", "ejs");
 
-//Web Endpoint
-app.get('/addBook', displayResults);
-app.get('/selectBooks', selectAllBooksFromDatabase);
+//Web Endpoints
+app.post('/addBook', displayResults);
+app.post('/selectBooks', selectAllBooksFromDatabase);
+app.post('/stats', displayStatistics);
 app.listen(port, () => console.log(`Application is Listening on port ${port}!`));
 
 function displayResults (req, res) {
@@ -17,15 +21,33 @@ function displayResults (req, res) {
     selectLatestBookFromDatabase(req, res);
 }
 
+function displayStatistics (req, res) {
+    var statsRequest = req.body.statOption;
+    var SQL = "";
+    console.log(req.body.statOption + "<-- Selected Option");
+    switch (statsRequest) {
+        case "Number of Books" :
+            SQL = "SELECT COUNT(*) FROM BOOK";
+            break;
+        case "Favorite Author" :
+            SQL = "SELECT author FROM book GROUP BY author ORDER BY COUNT(*) DESC LIMIT 1";
+            break;
+        case "Favorite Genre" :
+            SQL = "SELECT genre FROM book GROUP BY genre ORDER BY COUNT(*) DESC LIMIT 1";
+            break;
+    }   
+    selectFromDatabase(req, res, SQL);
+}
+
 function addNewBookToDatabase (req, res) {
 const connectionString = process.env.DATABASE_URL || 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
 const pool = new Pool({connectionString: connectionString});
-var SQL = "INSERT INTO Book (Title, Author, Genre) VALUES ('" + String(req.query.bookTitle) + "', '" + String(req.query.bookAuthor) + "', '" + String(req.query.bookGenre) + "')";
+var SQL = "INSERT INTO Book (Title, Author, Genre) VALUES ('" + String(req.body.bookTitle) + "', '" + String(req.body.bookAuthor) + "', '" + String(req.body.bookGenre) + "')";
 console.log(SQL);
     pool.query(SQL, function(err, result) {
         // If an error occurred...
         if (err) {
-        console.log("Error in query: ")
+        console.log("Error in body: ")
         console.log(err);
         }
         // Log this to the console for debugging purposes.
@@ -33,31 +55,6 @@ console.log(SQL);
         console.log(result.rows);
     });
 } 
-
-function selectLatestBookFromDatabase (req, res) {
-const connectionString = process.env.DATABASE_URL || 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
-const pool = new Pool({connectionString: connectionString});
-var SQL = "SELECT * FROM Book ORDER BY Book_ID DESC LIMIT 1";
-console.log(SQL);
-    pool.query(SQL, function(err, result) {
-        // If an error occurred...
-        if (err) {
-            console.log("Error in query: ")
-            console.log(err);
-        }
-        // Log this to the console for debugging purposes.
-        console.log("1 record selected");
-        console.log(result.rows);
-        console.log(result.rows.title);
-        let book = result.rows[0];
-        let bookJSON = JSON.stringify(book);
-        let title = String(book.title);
-        let author = String(book.author);
-        let genre = String(book.genre);
-        const params = {bookJSON: bookJSON, title: title, author: author, genre: genre};
-        res.render('results', params);
-    }); 
-}
 
 function selectAllBooksFromDatabase (req, res) {
 const connectionString = process.env.DATABASE_URL || 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
@@ -67,7 +64,7 @@ console.log(SQL);
     pool.query(SQL, function(err, result) {
         // If an error occurred...
         if (err) {
-            console.log("Error in query: ")
+            console.log("Error in body: ")
             console.log(err);
         }
         // Log this to the console for debugging purposes.
@@ -80,22 +77,47 @@ console.log(SQL);
     }); 
 }
 
-/* Ensure no duplicate entries TODO
-function selectBookFromDatabase (req, res, title) {
+function selectFromDatabase (req, res, SQL) {
     const connectionString = process.env.DATABASE_URL || 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
     const pool = new Pool({connectionString: connectionString});
-    var SQL = "SELECT * FROM Book ORDER BY Book_ID DESC LIMIT 1";
     console.log(SQL);
         pool.query(SQL, function(err, result) {
             // If an error occurred...
             if (err) {
-                console.log("Error in query: ")
+                console.log("Error in body: ")
                 console.log(err);
             }
             // Log this to the console for debugging purposes.
-            console.log("1 record selected");
-            console.log(result.rows);
-            return result.rows;
+            console.log("Records selected");
+            console.log(result.rows[0]);
+            let statsOption = result.rows[0];
+            let statsOptionJSON = JSON.stringify(statsOption);
+            res.end(statsOptionJSON);
         }); 
     }
-*/
+
+    // To render an EJS page
+    function selectLatestBookFromDatabase (req, res) {
+        const connectionString = process.env.DATABASE_URL || 'postgres://rbjaiulinstwdj:f189f4c73ef9dceabf44d5ce68ba252db3208b83f26bc277f4b6f24ca2893ba8@ec2-3-91-112-166.compute-1.amazonaws.com:5432/de9aegpl669co3?ssl=true';
+        const pool = new Pool({connectionString: connectionString});
+        var SQL = "SELECT * FROM Book ORDER BY Book_ID DESC LIMIT 1";
+        console.log(SQL);
+            pool.query(SQL, function(err, result) {
+                // If an error occurred...
+                if (err) {
+                    console.log("Error in body: ")
+                    console.log(err);
+                }
+                // Log this to the console for debugging purposes.
+                console.log("1 record selected");
+                console.log(result.rows);
+                console.log(result.rows.title);
+                let book = result.rows[0];
+                let bookJSON = JSON.stringify(book);
+                let title = String(book.title);
+                let author = String(book.author);
+                let genre = String(book.genre);
+                const params = {bookJSON: bookJSON, title: title, author: author, genre: genre};
+                res.render('results', params);
+            }); 
+        }
